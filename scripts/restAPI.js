2f02c8affae1f88651fcd4bb01efa8fe2fba220d
Federico <federico.obialero@gmail.com>
2018-11-29 19:15:14 +0100

    code cleanup

--- scripts/restAPI.js
+++ scripts/restAPI.js
@@ -1,28 +1,16 @@
 const atob = require('atob')
-const axios = require('axios')
 const fetch = require('node-fetch')
 const fs = require('fs')
 
-const headers = {
-  Authorization: `bearer f001853260be2447e36917a7ce4c7512c63f8562`
-}
+const baseUrl = 'https://api.github.com/repos/RigoBlock/rigoblock-monorepo/'
 
 const getData = async () => {
-  const masterBranch = await fetch(
-    'https://api.github.com/repos/RigoBlock/rigoblock-monorepo/branches/master',
-    {
-      headers
-    }
-  ).then(res => res.json())
+  const masterBranch = await fetchJSON(`${baseUrl}branches/master`)
   const treeSha = masterBranch.commit.sha
-  const allFiles = await fetch(
-    `https://api.github.com/repos/RigoBlock/rigoblock-monorepo/git/trees/${treeSha}?recursive=1`,
-    {
-      headers
-    }
-  ).then(res => res.json())
+  const allFiles = await fetchJSON(`${baseUrl}git/trees/${treeSha}?recursive=1`)
   const re = /\.md$/
   const markdowns = allFiles.tree.filter(file => file.path.match(re))
+
   markdowns.map(async markdown => {
     const { path, sha } = markdown
     const content = await fetchContentAndWrite(sha)
@@ -35,17 +23,19 @@ const getData = async () => {
       err ? console.error(err) : console.log(`${fileName} saved!`)
     )
   })
-  // const content = atob(response.content)
-  // console.log(content)
 }
 
-// const baseURL = 'https://api.github.com/repos/RigoBlock/rigoblock-monorepo/git/blobs/f6122066ecf983c9cd5bbce92997d5a2141408f3'
-
-const fetchContentAndWrite = async blobSha => {
-  const url = `https://api.github.com/repos/RigoBlock/rigoblock-monorepo/git/blobs/${blobSha}`
-  const response = await fetch(url, {
+const fetchJSON = url => {
+  const headers = {
+    Authorization: `bearer f001853260be2447e36917a7ce4c7512c63f8562`
+  }
+  return fetch(url, {
     headers
   }).then(res => res.json())
+}
+
+const fetchContentAndWrite = async blobSha => {
+  const response = await fetchJSON(`${baseUrl}git/blobs/${blobSha}`)
   return atob(response.content)
 }
 
