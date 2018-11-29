a7e26976eca66d919220aa39e6fc1d7008107ef1
Federico <federico.obialero@gmail.com>
2018-11-29 18:56:08 +0100

    initial commit 🌅

---
+++ output/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,11 @@
+---
+title: "Contracts"
+category: "packages"
+---
+
+resolves #CHANGEME
+
+#### :notebook: Overview
+
+#### :warning: Dependencies (optional)
+Depends on #CHANGEME
--- scripts/restAPI.js
+++ scripts/restAPI.js
@@ -1,12 +1,13 @@
 const atob = require('atob')
 const axios = require('axios')
 const fetch = require('node-fetch')
+const fs = require('fs')
+
+const headers = {
+  Authorization: `bearer f001853260be2447e36917a7ce4c7512c63f8562`
+}
 
 const getData = async () => {
-  const headers = {
-    Authorization: `bearer f001853260be2447e36917a7ce4c7512c63f8562`
-  }
-
   const masterBranch = await fetch(
     'https://api.github.com/repos/RigoBlock/rigoblock-monorepo/branches/master',
     {
@@ -22,11 +23,28 @@ const getData = async () => {
   ).then(res => res.json())
   const re = /\.md$/
   const markdowns = allFiles.tree.filter(file => file.path.match(re))
-  console.log(markdowns)
+  markdowns.map(async markdown => {
+    const { path, sha } = markdown
+    const content = await fetchContentAndWrite(sha)
+    const fileName = path.split('/').pop()
+    const data =
+      `---\ntitle: "Contracts"\ncategory: "packages"\n---\n\n` + content
+    return fs.writeFile(`output/${fileName}`, data, 'utf8', err =>
+      err ? console.error(err) : console.log(`${fileName} saved!`)
+    )
+  })
   // const content = atob(response.content)
   // console.log(content)
 }
 
 // const baseURL = 'https://api.github.com/repos/RigoBlock/rigoblock-monorepo/git/blobs/f6122066ecf983c9cd5bbce92997d5a2141408f3'
+
+const fetchContentAndWrite = async blobSha => {
+  const url = `https://api.github.com/repos/RigoBlock/rigoblock-monorepo/git/blobs/${blobSha}`
+  const response = await fetch(url, {
+    headers
+  }).then(res => res.json())
+  return atob(response.content)
+}
 
 getData()
