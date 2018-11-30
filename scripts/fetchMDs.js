87f1cdb6fea44631a09ecc5c6ba6d520389620ff
Federico <federico.obialero@gmail.com>
2018-11-30 12:40:07 +0100

    wip

--- scripts/fetchMDs.js
+++ scripts/fetchMDs.js
@@ -1,41 +1,46 @@
 const fetch = require('node-fetch')
 const fs = require('fs')
+const get = require('lodash/get')
 const { fetchJSON, postJSON } = require('./utils')
 
 // /branches/master
 const fetchREADMEs = async () => {
-  const GRAPQL_URL = 'https://api.github.com/graphql'
+  const GRAPHQL_URL = 'https://api.github.com/graphql'
+  const packages = await getPackageNames()
+  const linkRegexp = /(?<=\().*\.md(?=\))/g
+  // const linkRegexp = /\]\((.+\.md)\)/i // not working, only getting one link per file cause of no global
+  packages.map(async pkg => {
+    const query = `{
+      repository(owner: "RigoBlock", name: "rigoblock-monorepo") {
+        object(expression:"master:packages/${pkg}/README.md") {
+          ... on Blob {
+            text
+          }
+        }
+      }
+    }`
+    const response = await postJSON(GRAPHQL_URL, { query })
+    // fs.
+    const data =
+      response.data.repository.object && response.data.repository.object.text
+    const links = data.match(linkRegexp)
+    // console.log(links)
+    // if (links) {
+    //   links.map()
+    // }
+  })
+}
+
+const getPackageNames = async () => {
   const REST_URL = 'https://api.github.com/repos/RigoBlock/rigoblock-monorepo'
 
-  const treeUrl = await fetchJSON(`${REST_URL}/branches/master`).then(
-    res => res.commit.commit.tree.url
+  const packagesTreeUrl = await fetchJSON(`${REST_URL}/git/trees/HEAD`).then(
+    res => res.tree.filter(obj => obj.path === 'packages').pop().url
   )
-  const res = await fetchJSON(
-    'https://api.github.com/repos/RigoBlock/rigoblock-monorepo/git/trees/09f15fb23a9275cd50954cfcd6aa80d22f8dd648'
+  const packages = await fetchJSON(packagesTreeUrl).then(res =>
+    res.tree.map(pkg => pkg.path)
   )
-
-  console.log(res)
-  // const linkRegexp = /(?<=\().*\.md(?=\))/g
-  // const linkRegexp = /\]\((.+\.md)\)/i // not working, only getting one link per file cause of no global
-  // const packages = ['dapp', 'api', 'exchange-connector', 'contracts']
-  // packages.map(async pkg => {
-  //   const query = `{
-  //     repository(owner: "RigoBlock", name: "rigoblock-monorepo") {
-  //       object(expression:"master:packages/${pkg}/README.md") {
-  //         ... on Blob {
-  //           text
-  //         }
-  //       }
-  //     }
-  //   }`
-  //   const response = await postJSON(baseURL, { query })
-  //   // fs.
-  //   const data = response.data.repository.object.text
-  //   const links = data.match(linkRegexp)
-  //   if (links) {
-  //     links.map()
-  //   }
-  // })
+  return packages
 }
 
 fetchREADMEs()
