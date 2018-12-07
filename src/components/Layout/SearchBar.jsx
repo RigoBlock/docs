1359e40c4fa5bf71be9a6941ba81563f80f0f06b
Federico <federico.obialero@gmail.com>
2018-12-07 18:40:17 +0100

    fixed bug not showing search results

--- src/components/Layout/SearchBar.jsx
+++ src/components/Layout/SearchBar.jsx
@@ -2,7 +2,7 @@ import './SearchBar.scss'
 import { Index } from 'elasticlunr'
 import { navigateTo } from 'gatsby-link'
 import React, { useEffect, useRef, useState } from 'react'
-import queryString from 'query-string-es5'
+import qs from 'qs'
 
 export const SEARCH_URL = '/search'
 export const MINIMUM_QUERY_LENGTH = 3
@@ -12,9 +12,9 @@ const SearchBar = props => {
   const index = getOrCreateIndex(props)
   const searchEl = useRef(null)
   const [query, setQuery, handleChange] = useQuery()
-  const { q: queryParam, from } = queryString.parse(
-    typeof window !== 'undefined' && window.location.search
-  )
+  const { q: queryParam, from } = qs.parse(window.location.search, {
+    ignoreQueryPrefix: true
+  })
 
   // only on first mount, check if we are on search page, place focus on search bar
   // and add url query value to address bar if there is one
@@ -32,7 +32,7 @@ const SearchBar = props => {
 
   useEffect(
     () => {
-      if (queryParam >= MINIMUM_QUERY_LENGTH) {
+      if (queryParam.length >= MINIMUM_QUERY_LENGTH) {
         const results = index
           .search(queryParam, {
             title: { boost: 2 },
@@ -70,7 +70,7 @@ const useQuery = () => {
       const from = isSearchPage()
         ? ''
         : typeof window !== 'undefined' && window.location.pathname
-      const params = queryString.stringify({ q: query, from })
+      const params = qs.stringify({ q: query, from })
       navigateTo(`${SEARCH_URL}?${params}`)
     }
   }
