d41b2bd56fb95cb693d82f10e4baa6fbe5c5ac89
Luca Del Bianco <vshjxyz@gmail.com>
2019-01-21 10:53:32 +0100

    Minor fixes to styling and refactor to create scrollable table of contents

--- src/components/Layout/TableOfContents.jsx
+++ src/components/Layout/TableOfContents.jsx
@@ -1,6 +1,6 @@
 import './TableOfContents.scss'
 import Link from 'gatsby-link'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import changeCase from 'change-case'
 import classNames from 'classnames'
 import groupBy from 'lodash/groupBy'
@@ -23,40 +23,62 @@ const sortByTocClass = arr =>
   })
 
 const mapToLinks = arr => {
-  return sortByTocClass(arr).map((el, index) => {
-    const { frontmatter, fields } = el.entry.childMarkdownRemark
-    let { title, tocClasses } = frontmatter
-    if (tocClasses.length) {
-      title = title.replace(/ /g, '')
-    }
-    const classes = tocClasses.split(' ')
-    const iconType = iconTypes[classes.shift()]
-    const iconClasses = classNames(iconType, classes)
-    return (
-      <li className="entry-list-item" key={index}>
-        {!!tocClasses.length && <i className={iconClasses} />}
-        <Link to={fields.slug}>
-          <div className="entry-title">{title}</div>
-        </Link>
-      </li>
+  return sortByTocClass(arr)
+    .map((el, index) => {
+      const { frontmatter, fields } = el.entry.childMarkdownRemark
+      let { title, tocClasses } = frontmatter
+      if (tocClasses.length) {
+        title = title.replace(/ /g, '')
+      }
+      const classes = tocClasses.split(' ')
+      const iconType = iconTypes[classes.shift()]
+      const iconClasses = classNames(iconType, classes)
+      const ref = useRef(fields.slug)
+      return [
+        ref,
+        <li className="entry-list-item" key={index} ref={ref}>
+          {!!tocClasses.length && <i className={iconClasses} />}
+          <Link to={fields.slug}>
+            <div className="entry-title">{title}</div>
+          </Link>
+        </li>
+      ]
+    })
+    .reduce(
+      (acc, [ref, el]) => {
+        acc[0].push(ref)
+        acc[1].push(el)
+        return acc
+      },
+      [[], []]
     )
-  })
 }
 
-const organizeEntries = ([category, list], index) => {
-  const entries = mapToLinks(list)
-  return (
-    <React.Fragment key={index}>
+const organizeEntries = ([category, list]) => {
+  const [refs, entries] = mapToLinks(list)
+  return [
+    refs,
+    <React.Fragment key={category}>
       <h3>{changeCase.titleCase(category)}</h3>
       {entries}
     </React.Fragment>
-  )
+  ]
 }
 
 const DocList = ({ data }) => {
   const categories = Object.entries(
     groupBy(data, 'entry.childMarkdownRemark.frontmatter.subCategory')
   )
+  const ref = React.useRef()
+  const scrollTo = useSmoothScroll('x', ref)
+
+  let refs = []
+
+  useEffect(r => {
+    console.log('loaded', r)
+    console.log('el', refs)
+  }, [])
+
   const categoriesAndFolders = categories.map(([subCategory, values]) => {
     subCategory = formatCategory(subCategory)
     let groupByFolder = groupBy(
@@ -77,13 +99,15 @@ const DocList = ({ data }) => {
   const lists = categoriesAndFolders.map((el, index) => {
     const entries = mapToLinks(el.entries)
     const folders = Object.entries(el.folders).map(organizeEntries)
+    refs = folders.map(f => f[0]).reduce((acc, curr) => [...acc, ...curr], [])
+    const foldersElements = folders.map(f => f[1])
     return (
       <React.Fragment key={index}>
         {categories.length !== 1 && (
           <h4>{changeCase.titleCase(el.category)}</h4>
         )}
         {entries}
-        {folders}
+        {foldersElements}
       </React.Fragment>
     )
   })
@@ -94,6 +118,7 @@ const TableOfContents = ({ data }) => {
   if (!data) {
     return null
   }
+
   return (
     <div className="toc-wrapper">
       <ul className="package-list">
